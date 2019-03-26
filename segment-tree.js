692c07320b53fdd56aebe4e42fedd60c5104fc81
Valentine <bmaecmpo@gmail.com>
2019-03-26 09:13:10 +0500

    Segment tree

--- segment-tree.js
+++ segment-tree.js
@@ -11,6 +11,33 @@ function dummySegmentTree(array, fn, N) {
 }
 
 function segmentTree(array, fn, N) {
+  let tree = [];
+
+  function build(arr, v, tl, tr) {
+    if (tl == tr) {
+      tree[v] = arr[tl];
+    } else {
+      let tm = (tl + tr) / 2;
+      build(arr, v * 2, tl, tm);
+      build(arr, v * 2 + 1, tm + 1, tr);
+      tree[v] = fn(tree[v * 2], tree[v * 2 + 1]);
+    }
+  }
+
+  build(array, 1, 0, array.length - 1);
+
+  return function (from, to) {
+    function sum (v, tl, tr, l, r) {
+      if (l > r) return 0;
+      if (l == tl && r == tr) return tree[v];
+
+      let tm = (tl + tr) / 2;
+      return sum (v * 2, tl, tm, l, tm) + sum (v * 2 + 1, tm + 1, tr, tm, r);
+    }
+
+    return sum (1, 0, tree.length - 1, from, to);
+  }
+
   return neutralTree(N);
 };
 
