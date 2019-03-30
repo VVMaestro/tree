711707cd1e23839221a60e20ba734d256901d584
Valentine <bmaecmpo@gmail.com>
2019-03-30 08:51:04 +0500

    ...

--- segment-tree.js
+++ segment-tree.js
@@ -11,38 +11,116 @@ function dummySegmentTree(array, fn, N) {
 }
 
 function segmentTree(array, fn, N) {
-  let tree = [];
+  if (array.length == 0) return neutralTree(N);
 
+  //Инициация массива хранения дерева
+  const MAX_N = array.length;
+  let tree = new Array(MAX_N * 4);
+
+  for (let i = 0; i < tree.length; i++) {
+    tree[i] = N;
+  }
+
+  //Инициация функции построения
   function build(arr, v, tl, tr) {
     if (tl == tr) {
       tree[v] = arr[tl];
     } else {
-      let tm = (tl + tr) / 2;
-      build(arr, v * 2, tl, tm);
-      build(arr, v * 2 + 1, tm + 1, tr);
-      tree[v] = fn(tree[v * 2], tree[v * 2 + 1]);
+
+      let tm = parseInt((tl + tr) / 2);
+
+      build(arr, v*2, tl, tm);
+      build(arr, v*2+1, tm+1, tr);
+
+      tree[v] = fn(tree[v*2], tree[v*2+1]);
     }
   }
 
-  build(array, 1, 0, array.length - 1);
+  build(array, 1, 0, MAX_N-1);
 
+  //Возвращаемая функция
   return function (from, to) {
-    function sum (v, tl, tr, l, r) {
-      if (l > r) return 0;
+    if (from > to) throw new Error ('Правый индекс больше левого.');
+
+    function request (v, tl, tr, l, r) {
+      if (l > r) return N;
       if (l == tl && r == tr) return tree[v];
 
-      let tm = (tl + tr) / 2;
-      return sum (v * 2, tl, tm, l, tm) + sum (v * 2 + 1, tm + 1, tr, tm, r);
+      let tm = parseInt((tl + tr) / 2);
+
+      let argument1 = request(v*2, tl, tm, l, Math.min(r, tm));
+      let argument2 = request(v*2+1, tm+1, tr, Math.max(l, tm+1), r);
+
+      return fn(argument1, argument2);
     }
 
-    return sum (1, 0, tree.length - 1, from, to);
-  }
-
-  return neutralTree(N);
+    return request(1, 0, MAX_N-1, from, to-1);
+  }  
 };
 
 function recursiveSegmentTree(array, fn, N) {
-  return segmentTree(array, fn, N);
+  const MAX_N = array.length;
+  let tree = new Array(MAX_N * 4);
+  
+  //построение по вторым индексам
+  function buildY (vx, lx, rx, vy, ly, ry) {
+    if (ly == ry) {
+      if (lx == rx) tree[vx][vy] = array[lx][ly];
+      else tree[vx][vy] = fn(tree[vx*2][vy], tree[vx*2+1][vy]);
+    }
+
+    let my = parseInt ((ly + ry) / 2);
+    
+    buildY (vx, lx, rx, vy*2, ly, my);
+    buildY (vx, lx, rx, vy*2+1, my+1, ry);
+
+    tree[vx][vy] = fn(tree[vx][vy*2], tree[vx][vy*2+1]);
+  }
+
+  //построение по первым индексам
+  function buildX (vx, lx, rx) {
+    const MAX_M = array[vx].length;
+
+    if (lx != rx) {
+     let mx = parseInt(lx + rx) / 2;
+     buildX(vx*2, lx, mx);
+     buildX(vx*2+1, mx+1, rx);
+    }
+
+    buildY (vx, lx, rx, 1, 0, MAX_M-1);
+  }
+
+  buildX (1, 0, MAX_N - 1);
+
+  // return function (fromX, toX) {
+  //   return function (fromY, toY) {
+  //     function requestY (vx, vy, tly, try_, ly, ry) {
+  //       if (ly > ry) return N;
+  //       if (ly == tly && try_ == ry) return tree[vx][vy];
+
+  //       let tmy = parseInt((tly + try_) / 2);
+
+  //       let argument1 = (vx, vy*2, tly, tmy, ly, Math.min(ry, tmy));
+  //       let argument2 = (vx, vy*2+1, tmy+1, try_, Math.max(ly, tmy+1), ry);
+
+  //       return fn(argument1, argument2);
+  //     }
+
+  //     function requestX (vx, tlx, trx, lx, rx, ly, ly) {
+  //       if (lx > rx) return N;
+  //       if (lx == tlx && trx == rx) return requestY (vx, 1, 0, MAX_M, ly, ry);
+
+  //       let tmx = parseInt((tlx + trx) / 2);
+
+  //       let argument1 = requestX (vx*2, tlx, tmx, lx, Math.min(rx, tmx), ly, ry);
+  //       let argument2 = requestX (vx*2+1, tmx+1, trx, Math.max(lx, tmx+1), rx, ly, ry);
+  //       return fn(argument1, argument2);
+  //     }
+
+  //     return requestX (1, 0, MAX_M-1, fromX, toX, fromY, toY);
+  //   }
+  // }
+  // return segmentTree(array, fn, N);
 }
 
 function getElfTree(array) {
